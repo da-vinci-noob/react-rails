b43f0912690778016000e27361f09229c1dd4663
Shah Rukh <shah.xhtml@yahoo.com>
2023-03-06 03:17:04 +0530

    feat: Add Routes

---
+++ app/javascript/routes/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../components/Home'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
+
+export default (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
+)
